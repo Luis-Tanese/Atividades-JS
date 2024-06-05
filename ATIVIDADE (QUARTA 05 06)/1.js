e3ec715180fe345d82f3092ca1cec57329e9f289
Luis Tanese <luistanese@gmail.com>
2024-06-05 12:05:21 -0300

    Create 1.js

---
+++ ATIVIDADE (QUARTA 05 06)/1.js
@@ -0,0 +1,11 @@
+// 1) Faça um programa no qual o usuário digite dois números e mostre na tela a multiplicação desses números.
+
+var num1
+var num2
+
+num1 = Number(prompt("Digite o primeiro número a ser multiplicado: "))
+num2 = Number(prompt("Digite o segundo número a ser multiplicado: "))
+
+resultado = num1 * num2
+
+alert("A multiplicação dos dois numeros é: " + resultado)
