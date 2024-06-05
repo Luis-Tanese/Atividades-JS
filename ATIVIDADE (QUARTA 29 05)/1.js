95ddc52e4379a8773dd4734f6c9941ad0c62d2e5
Luis Tanese <luistanese@gmail.com>
2024-06-05 10:43:31 -0300

    Create 1.js

---
+++ ATIVIDADE (QUARTA 29 05)/1.js
@@ -0,0 +1,11 @@
+// 1) Criar um programa que receba o nome, idade e endereço de uma pessoa e mostre na tela esses dados.
+
+var nomePessoa
+var idadePessoa
+var enderecoPessoa
+
+nomePessoa = prompt('digite seu nome: ')
+idade = Number(prompt('digite sua idade: '))
+endereco = prompt('Digite seu enderço: ')
+
+alert(nome + "\n" + idade + "\n" + endereco)
