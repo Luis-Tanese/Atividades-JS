f590e3cd68461eea24deeee7708d96729bc663d0
Luis Tanese <luistanese@gmail.com>
2024-06-25 10:05:28 -0300

    Create 1.js

---
+++ ATIVIDADE (QUARTA 19 06)/1.js
@@ -0,0 +1,11 @@
+// 1) Crie um programa onde o usuário digite um número e informe se é positivo ou negativo.
+var numero
+
+numero = Number(prompt("   Digite um número (Negativo ou Positivo"))
+
+if(numero < 0){
+    alert("Seu número é negativo!")
+}
+else{
+    alert("Seu número é postivo!")
+}
